d33f1f8a759ac6fd2ba1bb08772e74522c57d7c5
ZhangYi <zhangyi@kideasoft.com>
2021-09-01 19:15:06 +0800

    change sum in request data

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,31 @@ impl PowermaxB4800 {
     pub fn voltages_1_12_req(addr: u8) -> [u8; 9] {
         let sum: u16 = [addr as u16, 0x24, 0x01, 0x00].iter().sum();
         let sum = sum.to_le_bytes();
-        [0x3A, addr, 0x24, 0x01, 0x00, sum[0], sum[1], 0x0D, 0x0A]
+        [0x3A, addr, 0x24, 0x01, 0x00, sum[1], sum[0], 0x0D, 0x0A]
     }
 
     pub fn voltages_13_24_req(addr: u8) -> [u8; 9] {
         let sum: u16 = [addr as u16, 0x25, 0x01, 0x00].iter().sum();
         let sum = sum.to_le_bytes();
-        [0x3A, addr, 0x25, 0x01, 0x00, sum[0], sum[1], 0x0D, 0x0A]
+        [0x3A, addr, 0x25, 0x01, 0x00, sum[1], sum[0], 0x0D, 0x0A]
     }
 
     pub fn state_req(addr: u8) -> [u8; 9] {
         let sum: u16 = [addr as u16, 0x2A, 0x01, 0x00].iter().sum();
         let sum = sum.to_le_bytes();
-        [0x3A, addr, 0x2A, 0x01, 0x00, sum[0], sum[1], 0x0D, 0x0A]
+        [0x3A, addr, 0x2A, 0x01, 0x00, sum[1], sum[0], 0x0D, 0x0A]
     }
 
     pub fn battery_balance_req(addr: u8) -> [u8; 9] {
         let sum: u16 = [addr as u16, 0xFE, 0x01, 0x00].iter().sum();
         let sum = sum.to_le_bytes();
-        [0x3A, addr, 0xFE, 0x01, 0x00, sum[0], sum[1], 0x0D, 0x0A]
+        [0x3A, addr, 0xFE, 0x01, 0x00, sum[1], sum[0], 0x0D, 0x0A]
     }
 
     pub fn product_info_req(addr: u8) -> [u8; 9] {
         let sum: u16 = [addr as u16, 0x2B, 0x01, 0x00].iter().sum();
         let sum = sum.to_le_bytes();
-        [0x3A, addr, 0x2B, 0x01, 0x00, sum[0], sum[1], 0x0D, 0x0A]
+        [0x3A, addr, 0x2B, 0x01, 0x00, sum[1], sum[0], 0x0D, 0x0A]
     }
 }
 
